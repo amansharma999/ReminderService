c250342907b837cf4cca684d45c341de9b179d26
amansharma999 <aman.gamestar999@gmail.com>
2024-09-29 03:00:59 +0530

    Setup of SMTP using nodemailer Done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { PORT } = require("./config/serverConfig");
 
 const bodyParser = require("body-parser");
+const { sendBasicEmail } = require("./services/email-service");
 
 const setupAndStartServer = () => {
   const app = express();
@@ -9,6 +10,12 @@ const setupAndStartServer = () => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.listen(PORT, () => {
+    // sendBasicEmail(
+    //   "support@Xcruz.com",
+    //   "aman.amansharmademo2000@gmail.com",
+    //   "Hello from Xcruz",
+    //   "Welcome to Xcruz! We are excited to have you on board."
+    // );
     console.log(`Server is running on port ${PORT}`);
   });
 };
